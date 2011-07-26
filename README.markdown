095febc2847bfb762a0ec7ef606e15066a8f8cb4
Paul Graham <graza@unknown-e4-ce-8f-48-3c-1a.lan>
2011-07-27 00:20:07 +0100

    Added echos to show what is going on Added README.markdown to show how to use

---
+++ README.markdown
@@ -0,0 +1,6 @@
+# To get started (bootstrap)
+
+{code}
+bash <(curl -s https://raw.github.com/grazanaut/MBP-Setup-Scripts/master/setup.sh)
+{code}
+
--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #
 #
 
+#
 # Set up chmod options for .ssh
+#
 echo 'Setting up permissions for .ssh folder and files'
+
+echo 'chmod 700 ~/.ssh'
 chmod 700 ~/.ssh
+
+echo 'chmod 600 ~/.ssh/id_rsa'
 chmod 600 ~/.ssh/id_rsa
+
+echo 'chmod 644 ~/.ssh/id_rsa.pub'
 chmod 644 ~/.ssh/id_rsa.pub
